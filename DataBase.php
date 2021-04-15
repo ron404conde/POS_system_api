690da7fe9242c0196598138ad0b1cf883ff9c91e
ron404conde <sollezarondel@gmail.com>
2021-04-15 10:32:58 +0800

    Added Procedure for Updating ProductInventory_Information

--- DataBase.php
+++ DataBase.php
@@ -43,8 +43,17 @@ class DataBase
         $product_discount = $this->prepareData($product_discount);
         $product_reorder = $this->prepareData($product_reorder);
 
-        $this->sql = "INSERT INTO " . $table . " (product_id, product_name, unit_in_stock, unit_price, discount_percentage, reorder_level) VALUES 
-                        ('" . $product_id . "','" . $product_name . "','" . $product_stocks . "','" . $product_price . "', '" . $product_discount . "', '" . $product_reorder . "')";
+        //$this->sql = "INSERT INTO " . $table . " (product_id, product_name, unit_in_stock, unit_price, discount_percentage, reorder_level) VALUES 
+        //                ('" . $product_id . "','" . $product_name . "','" . $product_stocks . "','" . $product_price . "', '" . $product_discount . "', '" . $product_reorder . "')";
+
+        // $this->sql = "BEGIN IF EXISTS(SELECT * FROM '" . $table . "' WHERE product_id = '" . $product_id . "') THEN UPDATE tblproduct SET product_id = '" . $product_id . "',
+        //                 product_name = '" . $product_name . "', unit_in_stock = '" . $product_stocks . "',
+        //                 unit_price = '" . $product_price . "', discount_percentage = '" . $product_discount . "',
+        //                 reorder_level = '" . $product_reorder . "' WHERE product_id = '" . $product_id . "'; ELSE INSERT INTO '" . $table . "' (product_id,
+        //                 product_name, unit_in_stock, unit_price, discount_percentage, reorder_level) VALUES ('" . $product_id . "', '" . $product_name . "',
+        //                 '" . $product_stocks . "', '" . $product_price . "', '" . $product_discount . "', '" . $product_reorder . "'); END IF; END";
+
+        $this->sql = "CALL spu_product_inventory('" . $product_id . "','" . $product_name . "','" . $product_stocks . "','" . $product_price . "', '" . $product_discount . "', '" . $product_reorder . "')";
 
         if (mysqli_query($this->connect, $this->sql)) {
             return true;
